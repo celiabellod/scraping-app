60a234c9b2296b26e4f626a7c57e92b125f53236
Célia Bellod <celia@ikuzo.fr>
2021-02-08 13:58:54 +0100

    Multiplicity ok

--- scraping/assets/js/main.js
+++ scraping/assets/js/main.js
@@ -1,27 +1,37 @@
 const btnMore = document.querySelector('.btn-more');
+if(btnMore){
+    btnMore.style.display = "none"
 
-btnMore.style.display = "none"
+    const typeSelect = document.querySelector('.type-select')
+    typeSelect.addEventListener('change', (e) => {
+        if(typeSelect.value == 'multiplicity'){
+            btnMore.style.display = "block"
+            const extractionSection = document.querySelectorAll('.extraction-section')
 
-const typeSelect = document.querySelector('.type-select')
-typeSelect.addEventListener('change', (e) => {
-    if(typeSelect.value == 'multiplicity'){
-        btnMore.style.display = "block"
-    }
-});
-const extractionSection = document.querySelectorAll('.extraction-section')
+            btnMore.addEventListener('click', (e) => {
 
-btnMore.addEventListener('click', (e) => {
+                const datas = document.querySelectorAll('.datas')
 
-    const datas = document.querySelectorAll('.datas')
+                const dataClone = datas[datas.length - 1].cloneNode(true);
 
-    const dataClone = datas[datas.length - 1].cloneNode(true);
+                const id = datas.length +1;
 
-    const id = datas.length +1;
+                dataClone.querySelector('#dataName').name = "dataName["+id+"]"
+                dataClone.querySelector('#dataType').name = "dataType["+id+"]"
+                dataClone.querySelector('#dataPath').name = "dataPath["+id+"]"
 
-    dataClone.querySelector('#dataName').name = "dataName["+id+"]"
-    dataClone.querySelector('#dataType').name = "dataType["+id+"]"
-    dataClone.querySelector('#dataPath').name = "dataPath["+id+"]"
+                const seeMoreDiv = document.querySelector('.see-more')
+                extractionSection[1].insertBefore(dataClone,seeMoreDiv)
+            });
 
-    const seeMoreDiv = document.querySelector('.see-more')
-    extractionSection[1].insertBefore(dataClone,seeMoreDiv)
-});
+        } else {
+            btnMore.style.display = "none"
+            const datas = document.querySelectorAll('.datas')
+            for(var i = 1; i < datas.length; i++){
+                datas[1].style.display = "none"
+
+            }
+        }
+    });
+}
+
